e3e82547d93f95187900e793701ded86e11dafc2
Louise <lillalalouise@gmail.com>
2018-10-21 16:30:57 +0200

    Added more tests for Player + inventory consisting of Strings.

--- src/main/java/com/inte/group4/Player.java
+++ src/main/java/com/inte/group4/Player.java
@@ -1,44 +1,69 @@
 package com.inte.group4;
 
 import java.awt.*;
+import java.util.ArrayList;
 
-public class Player extends Sprite{
-	
+public class Player extends Sprite {
+
 	private boolean alive;
+	private ArrayList<String> inventory;
+	private final static int MAX_INVENTORY = 5;
 
+	public Player(int ap, int hp) {
+		super(ap, hp);
+		alive = true;
+		inventory = new ArrayList<String>();
+	}
 
-    public Player(int ap, int hp){
-        super(ap,hp);
-        alive = true;
-    }
+	@Override
+	public void attack() {
 
+	}
 
-    @Override
-    public void attack() {
+	public void decreaseHp() {
 
-    }
+	}
 
-    public void decreaseHp(){
-
-    }
-    
-    public void decreaseHp(int attack) {
-    	setHp(getHp() - attack);
-    	if(getHp() <= 0) {
-    		setAlive(false);
-    	}
-    }
-
+	public void decreaseHp(int attack) {
+		setHp(getHp() - attack);
+		if (getHp() <= 0) {
+			setAlive(false);
+		}
+	}
 
 	public boolean isAlive() {
 		return alive;
 	}
 
-
 	public void setAlive(boolean alive) {
 		this.alive = alive;
 	}
-    
+
+	public void addToInventory(String object) {
+		if (inventory.size() < 5) {
+			inventory.add(object);
+		} else {
+			System.out.println("Cannot add item, inventory is full!");
+		}
+	}
+
+	public int getInventorySize() {
+		int inventorySize = inventory.size();
+		System.out.println("Inventory contains " + inventorySize + " items, out of " + MAX_INVENTORY);
+		return inventory.size();
+	}
+	
+	public void removeFromInventory(String objectToRemove) {
+		int index = inventory.indexOf(objectToRemove);
+		removeFromInventory(index);
+	}
+	
+	public void removeFromInventory(int indexToRemove) {
+		inventory.remove(indexToRemove);
+	}
+	
+	public String getItemByIndex(int index) {
+		return inventory.get(index);
+	}
 
 }
-
--- src/test/java/com/inte/group4/PlayerTest.java
+++ src/test/java/com/inte/group4/PlayerTest.java
@@ -6,36 +6,74 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
-	
+
 	Player newPlayer;
 
-//	@BeforeEach
-//	void setUp() throws Exception {
-//		newPlayer = new Player(5,5);
-//	}
-	
-	@Test
-	void createNewPlayerTest() {
+	@BeforeEach
+	void setUp() throws Exception {
 		newPlayer = new Player(10,100);
+	}
+
+	@Test
+	public void createNewPlayerTest() {
+		Player newHpPlayer = new Player(10, 100);
 		assertNotNull(newPlayer);
 	}
-	
+
 	@Test
-	void decreasePlayerHpTest() {
-		newPlayer = new Player(10,100);
+	public void decreasePlayerHpTest() {
+		Player newHpPlayer = new Player(10, 100);
 		int attack = 5;
 		newPlayer.decreaseHp(attack);
 		assertEquals(100 - attack, newPlayer.getHp());
 	}
-	
+
 	@Test
-	void decreasePlayerHpToNegativeTest() {
-		newPlayer = new Player(10, 100);
+	public void decreasePlayerHpToNegativeTest() {
+		Player newHpPlayer = new Player(10, 100);
 		int attack = 100;
 		newPlayer.decreaseHp(attack);
 		assertFalse(newPlayer.isAlive());
 	}
-
-
+	
+	@Test
+	public void fillPlayerInventory() {
+		newPlayer.addToInventory("Object 1");
+		newPlayer.addToInventory("Object 2");
+		newPlayer.addToInventory("Object 3");
+		newPlayer.addToInventory("Object 4");
+		newPlayer.addToInventory("Object 5");
+		newPlayer.addToInventory("Object 6");
+		assertEquals(5, newPlayer.getInventorySize());
+	}
+	
+	@Test
+	public void removeFromPlayerInventoryByObject() {
+		newPlayer.addToInventory("Object 1");
+		newPlayer.removeFromInventory("Object 1");
+		assertEquals(0, newPlayer.getInventorySize());
+	}
+	
+	@Test
+	public void removeFromPlayerInventoryByIndex() {
+		newPlayer.addToInventory("Object 1");
+		newPlayer.addToInventory("Object 2");
+		newPlayer.addToInventory("Object 3");
+		newPlayer.removeFromInventory(2);
+		assertEquals(2, newPlayer.getInventorySize());
+	}
+	
+	@Test
+	public void fillAndThenRemoveFromPlayerInventory() {
+		newPlayer.addToInventory("Object 1");
+		newPlayer.addToInventory("Object 2");
+		newPlayer.addToInventory("Object 3");
+		newPlayer.addToInventory("Object 4");
+		newPlayer.addToInventory("Object 5");
+		newPlayer.addToInventory("Object that shouldn't be added");
+		newPlayer.removeFromInventory("Object 5");
+		newPlayer.addToInventory("Object 6");
+		assertEquals("Object 6", newPlayer.getItemByIndex(4));
+	}
 
 }
