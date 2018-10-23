2eec53ebcbb5dc3c5e6b619477f45743a4320622
Minijaa <saho6409@student.su.se>
2018-10-23 11:43:58 +0200

    Added argument check for illegal pot-sizes

--- src/main/java/com/inte/group4/Potion.java
+++ src/main/java/com/inte/group4/Potion.java
@@ -1,20 +1,26 @@
 package com.inte.group4;
 
 public class Potion extends Item {
-    private int hpIncrease;
+    private static final int MASSIVE_POTION_VALUE = 90000000;
+    private static final int MAJOR_POTION_VALUE = 500;
+    private static final int MINOR_POTION_VALUE = 200;
+    private int currentHpIncrease;
 
-    public Potion(int hpIncrease) {
-        this.hpIncrease = hpIncrease;
+    public Potion(int currentHpIncrease) {
+        if (currentHpIncrease != MINOR_POTION_VALUE && currentHpIncrease != MAJOR_POTION_VALUE && currentHpIncrease != MASSIVE_POTION_VALUE) {
+            throw new IllegalArgumentException();
+        }
+        this.currentHpIncrease = currentHpIncrease;
     }
 
     private String determineType() {
         String type;
-        if (hpIncrease == 200) {
-            type = "Minor Healing: +" + hpIncrease + " HP";
-        } else if (hpIncrease == 500) {
-            type = "Major Healing: +" + hpIncrease + " HP";
+        if (currentHpIncrease == MINOR_POTION_VALUE) {
+            type = "Minor Healing: +" + currentHpIncrease + " HP";
+        } else if (currentHpIncrease == MAJOR_POTION_VALUE) {
+            type = "Major Healing: +" + currentHpIncrease + " HP";
         } else {
-            type = "Massive Healing: +" + hpIncrease + " HP";
+            type = "Massive Healing: +" + currentHpIncrease + " HP";
         }
         return type;
     }
