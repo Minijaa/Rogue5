be41ef8520cba2c0b228c83434509f0f8923434e
Louise <lillalalouise@gmail.com>
2018-10-30 17:08:29 +0100

    Added tests for coverage for Location, is now 100% :-)

--- src/main/java/com/inte/group4/Location.java
+++ src/main/java/com/inte/group4/Location.java
@@ -3,101 +3,98 @@ package com.inte.group4;
 import java.awt.*;
 
 public class Location {
-    private String locationText = "silence";
-    private Item treasure;
-    private Monster monster;
-    private Point position;
-    private char mapChar;
-    private boolean isVisited = false;
-    private static final char visitedLocationChar = '.';
-    private static final char unvisitedLocationChar = 'O';
-    private static final char playerAtLocationChar = 'X';
+	private String locationText = "silence";
+	private Item treasure;
+	private Monster monster;
+	private Point position;
+	private char mapChar;
+	private boolean isVisited = false;
+	private static final char visitedLocationChar = '.';
+	private static final char unvisitedLocationChar = 'O';
+	private static final char playerAtLocationChar = 'X';
 
-    public Location(String locationText, char mapChar) {
-        this.locationText = locationText;
-        this.mapChar = mapChar;
-        this.monster = null;
-        this.treasure = null;
-    }
+	public Location(Point p) {
+		// changed here
+		mapChar = unvisitedLocationChar;
+		position = p;
+	}
 
-    public Location(Point p) {
-        // changed here
-        mapChar = unvisitedLocationChar;
-        position = p;
-    }
+	public String getLocationText() {
+		return locationText;
+	}
 
-    public String getLocationText() {
-        return locationText;
-    }
+	public void setLocationText(String locationText) {
+		this.locationText = locationText;
+	}
 
-    public void setLocationText(String locationText) {
-        this.locationText = locationText;
-    }
+	public char getMapChar() {
+		return mapChar;
+	}
 
-    public char getMapChar() {
-        return mapChar;
-    }
+	public int setMapChar() {
+		if (monster != null) {
+			mapChar = monster.getMonsterChar();
+			return 2;
+		} else if (isVisited) {
+			mapChar = visitedLocationChar;
+			return 1;
+		} else {
+			mapChar = unvisitedLocationChar;
+			return 0;
+		}
+	}
 
-    public void setMapChar() {
-        if (monster != null) {
-            mapChar = monster.getMonsterChar();
-        }else if (isVisited) {
-            mapChar = visitedLocationChar;
-        }else{
-            mapChar = unvisitedLocationChar;
-        }
-    }
+	public void setTreasure(Item item) {
+		this.treasure = item;
+	}
 
+	public Item getTreasure() {
+		return treasure;
+	}
 
-    public void setTreasure(Item item) {
-        this.treasure = item;
-    }
+	public Item removeTreasure() {
+		Item removedTreasure = treasure;
+		treasure = null;
+		return removedTreasure;
+	}
 
-    public Item getTreasure() {
-        return treasure;
-    }
+	public Monster getMonster() {
+		return monster;
+	}
 
-    public void removeTreasure() {
-        treasure = null;
-    }
+	public Monster setMonster(Monster monster) {
+		this.monster = monster;
+		return monster;
+	}
 
-    public Monster getMonster() {
-        return monster;
-    }
+	public String playerEntersLocation() {
+		StringBuilder locationBuilder = new StringBuilder("");
+		if (monster != null) {
+			System.out.println("FIGHT!!!");
+			locationBuilder.append("FIGHT!!!");
+			return locationBuilder.toString();
+		}
 
-    public Monster setMonster(Monster monster) {
-        this.monster = monster;
-        return monster;
-    }
+		System.out.println(locationText);
+		locationBuilder.append(locationText);
+		return locationBuilder.toString();
 
-    public String playerEntersLocation() {
-        StringBuilder locationBuilder = new StringBuilder("");
-        if (monster != null) {
-            System.out.println("FIGHT!!!");
-            locationBuilder.append("FIGHT!!!");
-            return locationBuilder.toString();
-        }
+	}
 
-        System.out.println(locationText);
-        locationBuilder.append(locationText);
-        return locationBuilder.toString();
+	public void setVisited(boolean visited) {
+		isVisited = visited;
 
-    }
+	}
 
-    public void setVisited(boolean visited) {
-        isVisited = visited;
+	public boolean isVisited() {
+		return isVisited;
+	}
 
-    }
+	public void setPlayerAtLocation() {
+		mapChar = playerAtLocationChar;
+	}
 
-    public boolean isVisited() {
-        return isVisited;
-    }
-
-    public void setPlayerAtLocation() {
-        mapChar = playerAtLocationChar;
-    }
-
-    public Point getPosition() {
-        return position;
-    }
+	public Point getPosition() {
+		return position;
+	}
 }
--- src/test/java/com/inte/group4/LocationTest.java
+++ src/test/java/com/inte/group4/LocationTest.java
@@ -9,29 +9,84 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LocationTest {
 
-    Location testLocation;
+	Location testLocation;
 
+	@Test
+	public void testPlayerEntersLocationWithMonster() {
+		Point p = new Point(0, 0);
+		testLocation = new Location(p);
+		Monster m = (Monster) new Ogre(p);
+		testLocation.setMonster(m);
 
-    @Test
-    public void testPlayerEntersLocationWithMonster() {
-        Point p = new Point(0, 0);
-        testLocation = new Location(p);
-        Monster m = (Monster) new Ogre(p);
-        testLocation.setMonster(m);
+		String actual = testLocation.playerEntersLocation();
+		assertEquals("FIGHT!!!", actual);
 
-        String actual = testLocation.playerEntersLocation();
-        assertEquals("FIGHT!!!", actual);
+	}
 
-    }
+	@Test
+	public void testPlayerEntersLocationWithNoMonster() {
+		Point p = new Point(0, 0);
+		testLocation = new Location(p);
 
-    @Test
-    public void testPlayerEntersLocationWithNoMonster() {
-        Point p = new Point(0, 0);
-        testLocation = new Location(p);
+		String actual = testLocation.playerEntersLocation();
+		assertEquals("silence", actual);
+	}
 
-        String actual = testLocation.playerEntersLocation();
-        assertEquals("silence", actual);
-    }
+	@Test
+	public void setLocationAsVisitedTest() {
+		testLocation = new Location(new Point(4, 5));
+		testLocation.setVisited(true);
+		boolean isLocationVisited = testLocation.isVisited();
+		assertTrue(isLocationVisited);
+	}
 
+	@Test
+	public void setMapCharMonsterTest() {
+		Point p = new Point(6, 7);
+		testLocation = new Location(p);
+		Worm testWorm = new Worm(p);
+		testLocation.setMonster(testWorm);
+		int returnChar = testLocation.setMapChar();
+		assertEquals(2, returnChar);
+	}
+
+	@Test
+	public void setMapCharUnvisitedTest() {
+		testLocation = new Location(new Point(6, 7));
+		int returnChar = testLocation.setMapChar();
+		assertEquals(0, returnChar);
+
+	}
+
+	@Test
+	public void setMapCharVisitedTest() {
+		testLocation = new Location(new Point(6, 7));
+		testLocation.setVisited(true);
+		int returnChar = testLocation.setMapChar();
+		assertEquals(1, returnChar);
+	}
+
+	@Test
+	public void getTreasureTest() {
+		testLocation = new Location(new Point(6, 7));
+		Potion treasure = new Potion(501);
+		testLocation.setTreasure(treasure);
+		assertEquals(treasure, testLocation.getTreasure());
+	}
+
+	@Test
+	public void getTreasureIsNullTest() {
+		testLocation = new Location(new Point(6, 7));
+		assertNull(testLocation.getTreasure());
+	}
+
+	@Test
+	public void removeTreasureTest() {
+		testLocation = new Location(new Point(6, 7));
+		Potion treasure = new Potion(501);
+		testLocation.setTreasure(treasure);
+		Item removedTreasure = testLocation.removeTreasure();
+		assertEquals(removedTreasure, treasure);
+	}
 
 }
