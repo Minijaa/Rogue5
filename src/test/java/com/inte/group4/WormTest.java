2e09885b05d4723f66d1d74a791873d5b2e2a968
Minijaa <saho6409@student.su.se>
2018-10-30 20:11:36 +0100

    Added longer movement test for worms.

--- src/test/java/com/inte/group4/WormTest.java
+++ src/test/java/com/inte/group4/WormTest.java
@@ -8,44 +8,54 @@ import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WormTest {
-	private Worm testWorm;
+    private Worm testWorm;
 
-	@BeforeEach
-	void setUp() {
-		testWorm = new Worm(new Point(1, 1));
-	}
+    @BeforeEach
+    void setUp() {
+        testWorm = new Worm(new Point(1, 1));
+    }
 
-	@Test
-	void moveMonsterTest() {
-		testWorm.setUpOrLeft(true); // Tell worm to travel upwards
-		Point newPosition = testWorm.generatePointForMonsterMovement();
-		Point expectedPoint = new Point(1, 2);
-		assertEquals(newPosition, expectedPoint);
-		testWorm.setUpOrLeft(false); // Tell worm to travel downwards
-		newPosition = testWorm.generatePointForMonsterMovement();
-		expectedPoint.y = 0;
-		assertEquals(newPosition, expectedPoint);
-	}
+    @Test
+    void moveMonsterTest() {
+        testWorm.setUpOrLeft(true); // Tell worm to travel upwards
+        Point newPosition = testWorm.generatePointForMonsterMovement();
+        Point expectedPoint = new Point(1, 2);
+        assertEquals(newPosition, expectedPoint);
+        testWorm.setUpOrLeft(false); // Tell worm to travel downwards
+        newPosition = testWorm.generatePointForMonsterMovement();
+        expectedPoint.y = 0;
+        assertEquals(newPosition, expectedPoint);
+    }
 
-	@Test
-	void testBoundaries() {
-		Worm lowerBoundaryWorm = new Worm(new Point(4, 0));
-		Worm upperBoundaryWorm = new Worm(new Point(4, 9));
+    @Test
+    void testBoundaries() {
+        Worm lowerBoundaryWorm = new Worm(new Point(4, 0));
+        Worm upperBoundaryWorm = new Worm(new Point(4, 9));
 
-		lowerBoundaryWorm.setUpOrLeft(false); // Tell worm to travel downwards
-		upperBoundaryWorm.setUpOrLeft(true); // Tell worm to travel upwards
-		Point lowerWormPosition = lowerBoundaryWorm.generatePointForMonsterMovement();
-		Point upperWormPosition = upperBoundaryWorm.generatePointForMonsterMovement();
+        lowerBoundaryWorm.setUpOrLeft(false); // Tell worm to travel downwards
+        upperBoundaryWorm.setUpOrLeft(true); // Tell worm to travel upwards
+        Point lowerWormPosition = lowerBoundaryWorm.generatePointForMonsterMovement();
+        Point upperWormPosition = upperBoundaryWorm.generatePointForMonsterMovement();
 
-		assertTrue(lowerWormPosition.equals(new Point(4, 1)) && upperWormPosition.equals(new Point(4, 8)));
-		assertTrue(lowerBoundaryWorm.getIsUpOrLeft());
-		assertFalse(upperBoundaryWorm.getIsUpOrLeft());
-	}
+        assertTrue(lowerWormPosition.equals(new Point(4, 1)) && upperWormPosition.equals(new Point(4, 8)));
+        assertTrue(lowerBoundaryWorm.getIsUpOrLeft());
+        assertFalse(upperBoundaryWorm.getIsUpOrLeft());
+    }
 
-	@Test
-	void toStringWormTest() {
-		Worm testWorm = new Worm(new Point(3, 2));
-		String expectedtoString = "Worm AP:400 HP:200 cords: 3:2";
-		assertEquals(expectedtoString, testWorm.toString());
-	}
+    @Test
+    void testLongerMove() {
+        testWorm.setUpOrLeft(true);
+        for (int i = 0; i < 30; i++) {
+            testWorm.setCurrentMonsterCords(testWorm.generatePointForMonsterMovement());
+        }
+        Point expectedPosition = new Point(1, 5);
+        assertEquals(expectedPosition, testWorm.getCurrentMonsterCords());
+    }
+
+    @Test
+    void toStringWormTest() {
+        Worm testWorm = new Worm(new Point(3, 2));
+        String expectedtoString = "Worm AP:400 HP:200 cords: 3:2";
+        assertEquals(expectedtoString, testWorm.toString());
+    }
 }
